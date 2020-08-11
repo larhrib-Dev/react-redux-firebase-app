8a92e0bde40e530b6f46d0fd9cbb8c57c827cc82
abdelaziz larhrib <abdelaziz.larhrib93@gmail.com>
2020-08-11 15:14:47 -0400

    create a project and establish the project structure

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ProjectSummary() {
+    return (
+         <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by the DevLarhrib</p>
+                    <p className="grey-text">3rd September</p>
+                </div>
+        </div>
+    )
+}
