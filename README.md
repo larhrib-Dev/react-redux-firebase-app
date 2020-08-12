6e227beb45f06f31cb4a6189d70f75c97420023e
abdelaziz larhrib <abdelaziz.larhrib93@gmail.com>
2020-08-12 13:29:10 -0400

    add login and sigup and create project

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 #####creation of a project based on react, redux, firebase#####
 
 
-Step 1 : establish the project structure.
+Step 1 : Establish the project structure.
 Step 2 : Create project details
+Step 3 : Add login and Signup
--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/layout/Navbar';
+import CreateProject from './components/projects/CreateProjects';
 import ProjectDetails from './components/projects/ProjectDetails';
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
        <Switch>
          <Route exact path='/' component={Dashboard} />
          <Route exact path='/project/:id' component={ProjectDetails} />
+         <Route exact path='/signin' component={SignIn} />
+         <Route exact path='/signup' component={SignUp} />
+         <Route exact path='/create' component={CreateProject} />
        </Switch>
      </Fragment>
     </BrowserRouter>
